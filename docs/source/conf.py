155a81ca6ef3b1c8dfefe2a33436378249e6ab6c
Enio <33031809+eniohandy@users.noreply.github.com>
2025-10-08 17:50:59 +0000

    ajustes para Markdwon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,3 +33,18 @@ html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+# atualizado para aceitar notação Markdown
+
+extensions = [
+    'myst_parser',
+    # other extensions...
+]
+
+myst_enable_extensions = [
+    "deflist",
+    "colon_fence",
+    "html_admonition",
+    "html_image",
+]
+
